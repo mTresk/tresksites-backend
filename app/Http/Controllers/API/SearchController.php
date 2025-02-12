321a426573b892c130519b653a1dc05229568dcd
Maxim Tresk <djtresk@gmail.com>
2025-02-12 22:40:11 +0500

    refactor: use search request

--- app/Http/Controllers/API/SearchController.php
+++ app/Http/Controllers/API/SearchController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SearchRequest;
+use App\Http\Resources\SearchResource;
 use App\Models\Work;
 
 class SearchController extends Controller
 {
     public function search(SearchRequest $request)
     {
-        return Work::search($request->input('keywords'))->get();
+        return SearchResource::collection(Work::search($request->input('keywords'))->get());
     }
 }
---
+++ app/Http/Resources/SearchResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Advantage;
+use App\Models\Work;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Work */
+class SearchResource extends JsonResource
+{
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+        ];
+    }
+}
